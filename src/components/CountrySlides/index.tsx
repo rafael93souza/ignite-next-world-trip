0870215dc9adbc75361e9dc5ef7db84ee7b10616
Rafael Souza <rafael93.souzasfc5@gmail.com>
2023-02-15 18:25:28 -0300

    feat: criado componente de slide de países

---
+++ src/components/CountrySlides/index.tsx
@@ -0,0 +1,30 @@
+import { Flex, Text } from "@chakra-ui/react";
+import { Slide } from "./slide";
+
+export function CountrySlidesConteiner() {
+    return (
+        <>
+            <Text
+                textAlign="center"
+                mt={{ xl: "1", lg: "1", md: "20", base: "20" }}
+                fontSize={["20", "36"]}
+            >
+                Cidades +100
+            </Text>
+
+            <Flex
+                w="100%"
+                flexWrap="wrap"
+                my="20px"
+                maxW={1480}
+                mx="auto"
+                h="auto"
+                p={["0", "0", "50"]}
+            >
+                <Slide />
+            </Flex>
+        </>
+    )
+}
+
+
